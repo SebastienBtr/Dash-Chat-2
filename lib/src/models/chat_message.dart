21749295e0e571c47bc446735954a3a129b3797a
shan-shaji <abrahamshanshaji@gmail.com>
2022-04-30 22:15:01 +0530

    feat: add more message status

--- lib/src/models/chat_message.dart
+++ lib/src/models/chat_message.dart
@@ -110,6 +110,12 @@ class MessageStatus {
         return MessageStatus.received;
       case 'pending':
         return MessageStatus.pending;
+      case 'sending':
+        return MessageStatus.sending;
+      case 'sended':
+        return MessageStatus.sended;
+      case 'error':
+        return MessageStatus.error;
       default:
         return MessageStatus.none;
     }
@@ -119,4 +125,7 @@ class MessageStatus {
   static const MessageStatus read = MessageStatus._internal('read');
   static const MessageStatus received = MessageStatus._internal('received');
   static const MessageStatus pending = MessageStatus._internal('pending');
+  static const MessageStatus sending = MessageStatus._internal('sending');
+  static const MessageStatus sended = MessageStatus._internal('sended');
+  static const MessageStatus error = MessageStatus._internal('error');
 }
