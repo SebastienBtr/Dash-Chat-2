f79571b636da5a70594e42673792fb59fbf64091
SebastienBtr <sebastien29520@gmail.com>
2022-07-06 12:13:58 +0200

    improve default send button padding

--- lib/src/widgets/input_toolbar/default_send_button.dart
+++ lib/src/widgets/input_toolbar/default_send_button.dart
@@ -4,11 +4,13 @@ part of dash_chat_2;
 Widget Function(Function send) defaultSendButton({
   required Color color,
   IconData icon = Icons.send,
+  EdgeInsets? padding,
 }) =>
     (Function fct) => InkWell(
           onTap: () => fct(),
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
+            padding: padding ??
+                const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
             child: Icon(
               Icons.send,
               color: color,
