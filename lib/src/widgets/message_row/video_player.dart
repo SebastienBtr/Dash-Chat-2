3a6fe2311ffa5be93742bf620376f50956237a51
SebastienBtr <sebastien29520@gmail.com>
2023-08-22 11:49:55 +0200

    fix deprecated function

--- lib/src/widgets/message_row/video_player.dart
+++ lib/src/widgets/message_row/video_player.dart
@@ -28,7 +28,7 @@ class VideoPlayerState extends State<VideoPlayer> {
   @override
   void initState() {
     super.initState();
-    _controller = vp.VideoPlayerController.network(widget.url)
+    _controller = vp.VideoPlayerController.networkUrl(Uri.parse(widget.url))
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized,
         // even before the play button has been pressed.
